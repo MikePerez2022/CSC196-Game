54579994f5991856d33ae06605dc98118bee366e
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2023-06-28 13:36:24 -0600

    added filing system

---
+++ Source/Engine/Core/FileIO.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <filesystem>
+#include <string>
+
+namespace Kiko
+{
+	std::string getFilePath();
+}
--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -1,12 +1,19 @@
+#include "../../Engine/Core/Random.h"
+#include "../../Engine/Core/FileIO.cpp"
 #include <iostream>
 
 using namespace std;
 
 int main() 
 {
-#ifdef _DEBUG
-	cout << "Hello World! \n";
-#endif 
+	cout << Kiko::getFilePath() << endl;
 
-	cout << "Hello World! \n";
+
+	kiko::seedRandom((unsigned int)time(nullptr));
+
+	for (int i = 0; i < 10; i++) {
+
+		cout << kiko::random(5, 20) << endl;
+
+	}
 }
